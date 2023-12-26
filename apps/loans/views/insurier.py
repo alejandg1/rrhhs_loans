d3da4b772a2d548912e2a3c70af0b27e673ba3b7
Alejandro <josealejandrogomezparedes@gmail.com>
2023-12-26 10:21:58 -0500

    add: mixins in insurier

--- apps/loans/views/insurier.py
+++ apps/loans/views/insurier.py
@@ -2,16 +2,17 @@
 from django.urls import reverse_lazy
 from apps.loans.models import Insurier
 from apps.loans.forms.insurier import InsurierForm
-
+from apps.security.mixins.mixins import ListViewMixin, CreateViewMixin, UpdateViewMixin, DeleteViewMixin, PermissionMixin
 from django.views.generic import CreateView, ListView, UpdateView, DeleteView
 from django.db.models import Q
 from apps.security.mixins.mixins import ListViewMixin, CreateViewMixin, UpdateViewMixin, DeleteViewMixin
 
 
-class InsurierListView(ListViewMixin, ListView):
+class InsurierListView(PermissionMixin, ListViewMixin, ListView):
     model = Insurier
     template_name = 'insurier/list.html'
     context_object_name = 'insurier'
+    permission_required = 'view_insurier'
     paginate_by = 2
     query = None
 
@@ -20,19 +21,18 @@ class InsurierListView(ListViewMixin, ListView):
         q1 = self.request.GET.get('q1')  # ver
         if q1 is not None:
             self.query.add(Q(name__icontains=q1), Q.AND)
-        # q2 = self.request.GET.get('q2') # ver
-        # if q2 is not None:
-        #     query.add(Q(estado__icontains=q2), Q.AND)
         return self.model.objects.filter(self.query).order_by('id')
 
     def get_context_data(self, **kwargs):
         context = super().get_context_data(**kwargs)
         context['title'] = 'Aseguradoras'
         context['create_url'] = reverse_lazy('loans:insurier_create')
+        context['permission_add'] = context['permissions'].get(
+            'add_insurier', '')
         return context
 
 
-class InsurierCreateView(CreateViewMixin, CreateView):
+class InsurierCreateView(PermissionMixin, CreateViewMixin, CreateView):
     model = Insurier
     template_name = 'insurier/form.html'
     form_class = InsurierForm
@@ -46,7 +46,7 @@ class InsurierCreateView(CreateViewMixin, CreateView):
         return context
 
 
-class InsurierUpdateView(UpdateViewMixin, UpdateView):
+class InsurierUpdateView(PermissionMixin, UpdateViewMixin, UpdateView):
     model = Insurier
     template_name = 'insurier/form.html'
     form_class = InsurierForm
@@ -60,7 +60,7 @@ class InsurierUpdateView(UpdateViewMixin, UpdateView):
         return context
 
 
-class InsurierDeleteView(DeleteViewMixin, DeleteView):
+class InsurierDeleteView(PermissionMixin, DeleteViewMixin, DeleteView):
     model = Insurier
     template_name = 'insurier/delete.html'
     success_url = reverse_lazy('loans:insurier_list')
